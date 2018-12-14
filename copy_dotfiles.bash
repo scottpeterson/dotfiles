21d141ca57f24bbadc08c73d67f7ff25732b8d40
Scott Peterson <scott.peterson4@gmail.com>
2018-12-13 21:49:44 -0800

    copy_dotfiles script

---
+++ copy_dotfiles.bash
@@ -0,0 +1,7 @@
+cp .aliases ~/.aliases
+cp .bash_profile ~/.bash_profile
+cp .bash_prompt ~/.bash_prompt
+cp .bashrc ~/.bashrc
+cp .gitconfig ~/.gitconfig
+cp .profile ~/.profile
+cp .vimrc ~/.vimrc

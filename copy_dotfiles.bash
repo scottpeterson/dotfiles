b7f6b4cc723827cd7ea8b9724c5d8c5e19ffe8c0
Scott Peterson <speterson@michiganlabs.com>
2019-07-22 14:23:07 -0400

    changes

--- copy_dotfiles.bash
+++ copy_dotfiles.bash
@@ -2,6 +2,6 @@ cp .aliases ~/.aliases
 cp .bash_profile ~/.bash_profile
 cp .bash_prompt ~/.bash_prompt
 cp .bashrc ~/.bashrc
-cp .gitconfig ~/.gitconfig
+# cp .gitconfig ~/.gitconfig
 cp .profile ~/.profile
 cp .vimrc ~/.vimrc

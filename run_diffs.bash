207a6882492b501200448a61e626593605789adb
Scott Peterson <scott.peterson4@gmail.com>
2020-11-17 19:53:57 -0500

    Added diffs bash and upload local bash

---
+++ run_diffs.bash
@@ -0,0 +1,6 @@
+diff .aliases ~/.aliases
+diff .bash_profile ~/.bash_profile
+diff .bash_prompt ~/.bash_prompt
+diff .bashrc ~/.bashrc
+diff .profile ~/.profile
+diff .vimrc ~/.vimrc
---
+++ upload_local_dotfiles.bash
@@ -0,0 +1,6 @@
+cp ~/.aliases .aliases
+cp ~/.bash_profile .bash_profile
+cp ~/.bash_prompt .bash_prompt
+cp ~/.bashrc .bashrc
+cp ~/.profile .profile
+cp ~/.vimrc .vimrc

d7d5d8626a4a7345e6bb49eed821ae2b25c46f03
Scott Peterson <scott@headway.io>
2023-09-02 14:48:29 -0400

    Changes to dotfiles
    Updating all_brew.sh

--- .vimrc
+++ .vimrc
@@ -1,2 +1,3 @@
 set background=dark
 syntax on
+
--- all_brew.bash
+++ all_brew.bash
@@ -4,7 +4,7 @@ echo Install and Set San Francisco as System Font
 ruby -e "$(curl -fsSL https://raw.github.com/wellsriley/YosemiteSanFranciscoFont/master/install)"
 
 echo Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo Homebrew Maintenance
 brew doctor
@@ -15,32 +15,10 @@ brew tap phinze/cask
 brew install brew-cask
 
 echo Allllll the installs
-declare -a cask_apps=(
-  '1password'
-  'wget'
-  'iterm2'
-  'transmit'
-  'sublime-text'
-  'github'
-  'visual-studio-code'
-  'git'
-  'tree'
-  # 'postgresql'
-  # 'go'
-  # 'golang'
-  # 'node'
-  # 'ant'
-  # 'ffmpeg'
-  # 'eigen'
-  # 'opencv'
-  # 'java'
-)
-
-for app in "${cask_apps[@]}"; do
-  brew cask install "$app"
-done
+brew bundle
 
 # cleanup
 brew cleanup --force
 rm -f -r /Library/Caches/Homebrew/*
 
+brew cu --all --cleanup -y -v ; do_all

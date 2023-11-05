a103d2f364d1f8adadf26fe50c4246b61e322b4b
Scott Peterson <scott@headway.io>
2023-11-05 14:12:57 -0500

    Updated all_brew

--- all_brew.bash
+++ all_brew.bash
@@ -4,41 +4,14 @@ echo Install and Set San Francisco as System Font
 ruby -e "$(curl -fsSL https://raw.github.com/wellsriley/YosemiteSanFranciscoFont/master/install)"
 
 echo Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo Homebrew Maintenance
 brew doctor
 brew update
 brew outdated
 brew upgrade
-brew tap phinze/cask
 brew install brew-cask
-
-echo Allllll the installs
-declare -a cask_apps=(
-  '1password'
-  'wget'
-  'iterm2'
-  'transmit'
-  'sublime-text'
-  'github'
-  'visual-studio-code'
-  'git'
-  'tree'
-  # 'postgresql'
-  # 'go'
-  # 'golang'
-  # 'node'
-  # 'ant'
-  # 'ffmpeg'
-  # 'eigen'
-  # 'opencv'
-  # 'java'
-)
-
-for app in "${cask_apps[@]}"; do
-  brew cask install "$app"
-done
 
 # cleanup
 brew cleanup --force
